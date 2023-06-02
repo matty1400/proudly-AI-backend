aa1cb7d8073029fd539f21b73d385450e4dbce97
matty1400 <matijs.eraly@live.be>
2023-06-02 13:53:34 +0200

    test hook

--- www/app/Http/Controllers/WebhookController.php
+++ www/app/Http/Controllers/WebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\http\Controllers\DeviceController;
 
 class WebhookController extends Controller
 {
@@ -12,7 +13,34 @@ class WebhookController extends Controller
         $payload = $request->all();
 
         if($payload['exitMessage']=='finished'){
-            echo 'finished';
+            $url = 'https://matijseraly.be/api/user';
+
+            $headers = [
+                'username' => 'JohnDoe',
+                'mail' => 'john.doe@example.com',
+                'password' => 'secretpassword'
+            ];
+            
+            $options = [
+                'http' => [
+                    'method' => 'POST',
+                    'header' => "Content-Type: application/json\r\n",
+                    'content' => json_encode($headers)
+                ]
+            ];
+            
+            $context = stream_context_create($options);
+            $response = file_get_contents($url, false, $context);
+            
+            if ($response === false) {
+                // Handle error
+                echo 'Error occurred while making the request.';
+            } else {
+                $responseData = json_decode($response, true);
+                // Process the response data
+                echo $responseData['message'];
+            }
+            
         }
         else{
             echo 'not done';
