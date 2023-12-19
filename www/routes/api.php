fea6eb1b91f68f20f1ecaabf48281b8e7863dfa5
matty1400 <matijs.eraly@live.be>
2023-12-19 10:02:14 +0100

    yed

--- www/routes/api.php
+++ www/routes/api.php
@@ -39,7 +39,7 @@ Route::get('follower/{userId?}', [DeviceController::class, 'getFollowingUsers'])
 Route::get('followernames/{userId?}', [DeviceController::class, 'getFollowerNames']);
 
 
-Route::get('/send-welcome-email/{email}/{name}', [DeviceController::class, 'sendWelcomeEmail']);
+Route::get('sendMail/{email}/{name}', [DeviceController::class, 'sendWelcomeEmail']);
 
 
 
