0316c7bae6fad80be659ac8cf520d3c15f315403
matty1400 <matijs.eraly@live.be>
2023-12-25 21:52:43 +0100

    source code

---
+++ www/storage/framework/views/e98add493c2322b54c8ba5c70320c4f2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome Email</title>
+</head>
+<body>
+    <h1>Welcome to The Proudly Platform!</h1>
+    <p>Dear <?php echo e($data['name']); ?>,</p>
+    <p>Welcome to the Proudly Sales Navigator! We are excited to have you on board.</p>
+    <p>Thank you for joining us.</p>
+    <p>Regards,<br>
+    The Proudly Team</p>
+</body>
+</html>
+<?php /**PATH C:\Users\matij\Documents\GitHub\proudly-AI-backend\www\resources\views/emails/welcome.blade.php ENDPATH**/ ?>
